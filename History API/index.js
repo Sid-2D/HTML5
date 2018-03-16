e9a74bcffc362bdb9930b7c86913c66e7f3ea6d4
Sid <sid6327@gmail.com>
2018-03-16 23:49:56 +0530

    Better example for history API.

--- History API/index.js
+++ History API/index.js
@@ -1,7 +1,29 @@
+let state = 0
+
 function push() {
-  history.pushState(
-    {state:1, rand:Math.random()}, 
-    "State 1", 
-    "?state=1"
-  ); // logs {state:1,rand:"some random value"}, "State 1", "?state=1""
+	history.pushState(
+		{ state: ++state },
+		"State " + state,
+		"?state=" + state
+	)
+	document.getElementById('display').innerHTML = state
+}
+
+function pop() {
+	window.history.back()
+}
+
+// To be tested
+function go() {
+	window.history.go(3)
+	history.replaceState({page: 3}, "title 3", "?page=3")
+}
+
+function reduceState() {
+	document.getElementById('display').innerHTML = --state
+}
+
+window.onpopstate = e => {
+	console.log("location: " + document.location + ", state: " + JSON.stringify(event.state))
+	reduceState()
 }
