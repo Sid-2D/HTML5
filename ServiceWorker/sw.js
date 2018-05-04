652b7de23a0edbff0e112f53d86c1a9a70f75663
Sid <sid6327@gmail.com>
2018-05-04 23:42:13 +0530

    Caching using service worker.

--- ServiceWorker/sw.js
+++ ServiceWorker/sw.js
@@ -1,3 +1,23 @@
+let CACHE_NAME = 'sw-cache-v2'
+let urlsToCache = [
+	'/main.js'
+]
+
 self.addEventListener('install', e => {
 	console.log('At service worker')
+	e.waitUntil(
+		caches.open(CACHE_NAME)
+			  .then(cache => {
+			  		console.log('Opened cache')
+			  		return cache.addAll(urlsToCache)
+			  })
+	)
+	console.log('Caching complete')
+})
+
+self.addEventListener('fetch', e => {
+	console.log(e.request)
+	e.respondWith(
+		fetch(e.request)
+	)
 })
