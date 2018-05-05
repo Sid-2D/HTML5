267dbe1984741c40a58e304d5d5b8310f8ebbf93
Sid <sid6327@gmail.com>
2018-05-05 23:09:22 +0530

    Adding code for cache and return requests.

--- ServiceWorker/sw.js
+++ ServiceWorker/sw.js
@@ -18,6 +18,26 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
 	console.log(e.request)
 	e.respondWith(
-		fetch(e.request)
+		caches.match(e.request)
+			  .then(response => {
+			  		if (response) {
+			  			return response
+			  		}
+
+			  		let fetchRequest = e.request.clone()
+			  		return fetch(fetchRequest).then(response => {
+			  			if (!response || response.status !== 200 || response.type !== 'basic') {
+			  				return response
+			  			}
+			  			let responseToCache = response.clone()
+
+			  			caches.open(CACHE_NAME)
+			  				  .then(cache => {
+			  				  		cache.put(e.request, responseToCache)
+			  				  })
+
+			  			return response
+			  		})
+			  })
 	)
 })
